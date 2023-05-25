edc80ef2511c3dd6083f9653881dfbc27b3adfed
CherepanovVS <89967181+Che3108@users.noreply.github.com>
2023-05-26 07:07:09 +0800

    Update server.py

--- server/server.py
+++ server/server.py
@@ -33,34 +33,23 @@ def upload(file: UploadFile = File(...)):
             for source_file in files_name:
                 with open(os.path.join(work_folder, source_file), 'r', encoding='utf-8') as f:
                     raw_data = pars_txt_file(f.read())
-                data.append(raw_data)
+                data += raw_data
             results[source.split("2")[0]] = data
         
-        # пересобираем результарующий словарь
-        results_rebild = dict()
-        for k, v in results.items():
-            sats = list()
-            for d in v:
-                sats += list(d.keys())
-            sats = list(set(sats))
-            sats.sort()
-            results_rebild[k] = dict()
-            for st in sats:
-                results_rebild[k][st] = {"Access":[], "Start Time (UTCG)":[], "Stop Time (UTCG)":[], "Duration (sec)":[]}
-            for d in v:
-                for d_k, d_v in d.items():
-                    results_rebild[k][d_k]["Access"] += d_v["Access"]
-                    results_rebild[k][d_k]["Start Time (UTCG)"] += d_v["Start Time (UTCG)"]
-                    results_rebild[k][d_k]["Stop Time (UTCG)"] += d_v["Stop Time (UTCG)"]
-                    results_rebild[k][d_k]["Duration (sec)"] += d_v["Duration (sec)"]
-        with open("resunt_rebild_pars.json", 'w', encoding="utf-8") as f:
-            json.dump(results_rebild, f, indent=4, ensure_ascii=True)
+        # пересобираем результат парсинга в df
+        Facility_df = pd.DataFrame(data=results["Facility"], columns=["Station", "Satellite", "Access", "Start Time (UTCG)", "Stop Time (UTCG)", "Duration (sec)"])
+        Russia_df = pd.DataFrame(data=results["Russia"], columns=["Station", "Satellite", "Access", "Start Time (UTCG)", "Stop Time (UTCG)", "Duration (sec)"])
+        print(Facility_df.head())
+        print(Russia_df.head())
+        
+        with open("result_pars.json", 'w', encoding="utf-8") as f:
+            json.dump(results, f, indent=4, ensure_ascii=True)
 
 
 
         os.system(f'rm -fr {TEMP_FOLDER + "/*"}')
 
-    return {"message": f"Successfully uploaded {file.filename}"}
+    return {"message": results}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8008)
