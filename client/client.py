754084be3f926b20b580265641c4e6a6348ae9dc
CherepanovVS <89967181+Che3108@users.noreply.github.com>
2023-05-26 18:55:45 +0800

    Update client.py

--- client/client.py
+++ client/client.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
 
 import requests
+import pandas as pd
 
-URL = 'http://127.0.0.1:8008/upload'
+
+URL = 'http://62.109.6.118:8008/upload'
+ARHIVE_name = 'DATA_Files (1).zip'
+
 files = [
-    ('file', ('DATA_Files (1).zip', open('DATA_Files (1).zip', 'rb'))),
+    ('file', (ARHIVE_name, open(ARHIVE_name, 'rb'))),
 ]
 response = requests.post(URL, files=files)
-print(response.text)
+
+
+if __name__ == "__main__":
+    def base_logic(data:dict):
+        Facility_df = pd.DataFrame(data=data["Facility"], columns=["Station", "Satellite", "Access", "Start Time (UTCG)", "Stop Time (UTCG)", "Duration (sec)"])
+        Russia_df = pd.DataFrame(data=data["Russia"], columns=["Station", "Satellite", "Access", "Start Time (UTCG)", "Stop Time (UTCG)", "Duration (sec)"])
+        return (Facility_df, Russia_df)
+    
+    # Facility_df - это видимость станциями
+    # Russia_df - это спутники пролетают над РФ
+    Facility_df, Russia_df = base_logic(eval(response.text))
+    print(Facility_df.head())
+    print(Russia_df.head())
